d2d85a91012b40e1584a1623d6a05df919200205
misanc21 <misanc21@gmail.com>
2020-09-30 14:44:01 -0500

    adding title

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { makeStyles, Paper, Grid, Typography } from '@material-ui/core';
+import mask from '../img/mask.png'
+
 
 const Home = () => {
-    return ( <h2>
-        Home
-    </h2> );
+    const classes = useStyles()
+    return (
+        <div className={classes.root}>
+            <Grid container>
+                <Grid item xs={12} className={classes.titleContainer}>
+                    <Typography variant="h1" className={classes.titleText}color="secondary">What's Happening with</Typography>
+                    <Typography variant="h1" className={classes.titleText}color="secondary">Covid-19?</Typography>
+                    <img src={mask} alt="mask" className={classes.mask}/>
+                    <Typography variant="h1" className={classes.hashtag}color="primary">#WearAMask</Typography>
+                </Grid>
+            </Grid>
+        </div>
+    );
 }
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+    titleContainer: {
+        width: '100%',
+        textAlign: 'center'
+    },
+    titleText: {
+        fontFamily: 'Bebas neue',
+        [theme.breakpoints.up('sm')]: {
+            fontSize: '8rem'
+        }
+    },
+    mask: {
+        maxWidth:'250px',
+
+    },
+    hashtag: {
+        fontFamily: 'Bebas neue',
+        fontSize: '2rem'
+    }
+  }));
  
 export default Home;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import { makeStyles, AppBar, Toolbar, Typography,IconButton } from '@material-ui/core';
+import React, {useState} from 'react';
+import { makeStyles, AppBar, Toolbar, Typography,IconButton, Drawer, List, ListItemText, ListItem, ListItemIcon} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
 import {Link} from 'react-router-dom'
 
 const Nav = () => {
     const classes = useStyles();
+    const [dra, setDra] = useState(false)
+
+    const handleToggle = (open) => {
+        setDra(open)
+    }
+
     return (
         <div className={classes.root}>
             <AppBar position="static" color="primary">
                 <Toolbar className={classes.appBar}>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={()=> handleToggle(true)}>
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h4" className={classes.title}>
@@ -25,6 +33,25 @@ const Nav = () => {
                     </div>
                 </Toolbar>
             </AppBar>
+            <Drawer anchor="left" open={dra} onClose={() => handleToggle(false)}>
+            <List>
+                <Typography variant="h6" className={classes.DrawerTitle}>
+                    Menu
+                </Typography>
+                <ListItem>
+                    <Link to="/" className={classes.draLink} onClick={() => handleToggle(false)}>
+                        <ListItemIcon><HomeIcon/></ListItemIcon>
+                        <ListItemText>Home</ListItemText>
+                    </Link>
+                </ListItem>
+                <ListItem>
+                    <Link to="/pais" className={classes.draLink} onClick={() => handleToggle(false)}>
+                        <ListItemIcon><FlagIcon/></ListItemIcon>
+                        <ListItemText>By Country</ListItemText>
+                    </Link>
+                </ListItem>
+            </List>
+          </Drawer>
         </div>
     );
 }
@@ -33,6 +60,11 @@ const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
         fontFamily: 'Cabin',
+    },
+    menuButton:{
+        [theme.breakpoints.up('sm')]: {
+            display: 'none'
+        }
     },
     title: {
         marginRight: '2rem',
@@ -49,13 +81,39 @@ const useStyles = makeStyles((theme) => ({
         flexFlow: 'row nowrap',
         color: 'white',
         justifyContent: 'flex-start',
+        [theme.breakpoints.down('xs')]: {
+            display: 'none'
+        }
     },
     link: {
         textDecoration: 'none!important',
         color: 'white',
         marginRight: '1rem',
         marginLeft: '1rem',
-    }
+    },
+    list: {
+        width: 250,
+      },
+      fullList: {
+        width: 'auto',
+      },
+      DrawerTitle:{
+          textAlign: 'center'
+      },
+      draLink: {
+        width: '100%',
+        display: 'flex',
+        flexFlow: 'row nowrap',
+        textDecoration: 'none',
+        alignItems: 'center',
+        color: '#666666',
+        fontWeight: 'bold',
+    
+        '&:visited':{
+            color: '#666666',
+        }
+
+      }
 }));
 
 export default Nav;
